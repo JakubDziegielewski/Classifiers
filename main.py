0a63d4e672ebbf2505372461345e567f3b867179
JakubDziegielewski <kubadziegiel1@gmail.com>
2024-05-23 17:26:21 +0200

    improved Niave Bayes

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-from  src.NaiveBayes import NaiveBayes
+from src.NaiveBayes import NaiveBayes
 from sklearn.model_selection import train_test_split
 import pandas as pd
 import numpy as np
 from scipy.io import arff
 from sklearn.preprocessing import LabelEncoder
+from sklearn.naive_bayes import GaussianNB
 
 
-def run_naive_bayes(test_size, random_state, x, y, verbose=False, group_number=4):
+def run_naive_bayes(x, y, test_size=0.1, verbose=False, group_number=4, random_state=0):
     x_train, x_test, y_train, y_test = train_test_split(
         x, y, test_size=test_size, random_state=random_state
     )
@@ -47,7 +48,7 @@ def run_naive_bayes(test_size, random_state, x, y, verbose=False, group_number=4
     return good / total
 
 
-file = arff.loadarff("data/wdbc.arff")
+file = arff.loadarff("data/glass.arff")
 df = pd.DataFrame(file[0])
 
 
@@ -56,7 +57,55 @@ def encode_column(column):
     column = le.fit_transform(column)
     return column
 
-df["class"] = encode_column(df["class"])
-data = np.array(df.drop("class", axis=1))
 
-run_naive_bayes(0.1, 1, data, df["class"], group_number=4, verbose=True)
+df["class"] = encode_column(df["class"])
+X = np.array(df.drop("class", axis=1))
+y = np.array(df["class"])
+x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=0)
+
+
+run_naive_bayes(X, y, group_number=8, verbose=True, random_state=11)
+
+"""
+rfc = RandomForestClassifier()
+rfc.fit(x_train, y_train)
+pred = rfc.predict(x_test)
+print(sum(pred == y_test) / len(pred))
+
+group_number = 4
+x_train = np.loadtxt("data/X_train.txt", dtype = float)
+x_test = np.loadtxt("data/X_test.txt", dtype = float)
+y_train = np.loadtxt("data/y_train.txt", dtype = int)
+y_test = np.loadtxt("data/y_test.txt", dtype = int)
+verbose = True
+nb = NaiveBayes()
+nb.find_intervals(x_train, group_number)
+intervals = nb.intervals
+x_train = np.array(
+    [
+        NaiveBayes.data_discretization(features, intervals[i])
+        for i, features in enumerate(x_train.T)
+    ]
+).T
+x_test = np.array(
+    [
+        NaiveBayes.data_discretization(features, intervals[i])
+        for i, features in enumerate(x_test.T)
+    ]
+).T
+if verbose:
+    print(f"Testing discrete naive bayes classifier")
+    print("result of classification of the test set:")
+good = 0
+total = 0
+nb = NaiveBayes()
+nb.build_classifier(x_train, y_train - 1, group_number)
+for test_x, test_y in zip(x_test, y_test - 1):
+    prediction = nb.predict(test_x)
+    if verbose:
+        print(f"Prediction: {prediction}, True class: {test_y}")
+    if prediction == test_y:
+        good += 1
+    total += 1
+if verbose:
+    print(f"Accuracy: {good/total:.3f}")"""
--- src/NaiveBayes.py
+++ src/NaiveBayes.py
@@ -2,9 +2,6 @@ from collections import Counter
 import numpy as np
 import bisect
 
-from sklearn.datasets import load_iris
-from sklearn.model_selection import train_test_split
-
 
 class NaiveBayes:
     def __init__(self):
@@ -23,21 +20,23 @@ class NaiveBayes:
         for key in self.priors.keys():
             key_occurances = self.priors[key]
             self.priors[key] /= total
-            for i in range(train_features.shape[1]): #Laplace smoothing
+            for i in range(train_features.shape[1]):  # Laplace smoothing
                 for j in range(group_number):
-                    self.likelihoods[key, i, j] = (self.likelihoods[key, i, j] + 1) / (key_occurances + group_number)
-                    
-    def find_intervals(self, train_data, number_of_groups):
-        self.intervals = np.zeros(shape=(train_data.shape[1], number_of_groups - 1))
+                    self.likelihoods[key, i, j] = (self.likelihoods[key, i, j] + 1) / (
+                        key_occurances + group_number
+                    )
+
+    def find_intervals(self, train_data, group_number):
+        self.intervals = np.zeros(shape=(train_data.shape[1], group_number - 1))
         for i, features in enumerate(train_data.T):
             max_value = max(features)
             min_value = min(features)
-            section_size = (max_value - min_value) / number_of_groups
-            
+            section_size = (max_value - min_value) / group_number
+
             self.intervals[i] = np.array(
-                [min_value + section_size * j for j in range(1, number_of_groups)]
+                [min_value + section_size * j for j in range(1, group_number)]
             )
-            
+
     @staticmethod
     def data_discretization(data, intervals):
         return [bisect.bisect_right(intervals, x) for x in data]
