34c1641c4a62bb8b7a6b7f08209e346991a3fdfa
JakubDziegielewski <kubadziegiel1@gmail.com>
2024-06-04 19:52:30 +0200

    deleted unnecessary code

--- main.py
+++ main.py
@@ -6,37 +6,3 @@ def main():
 
 if __name__=="__main__":
     main()
-
-
-"""
-x_train = np.loadtxt("data/X_train.txt", dtype = float)
-x_test = np.loadtxt("data/X_test.txt", dtype = float)
-y_train = np.loadtxt("data/y_train.txt", dtype = int)
-y_test = np.loadtxt("data/y_test.txt", dtype = int)
-verbose = True
-nb = NaiveBayes()
-intervals = find_intervals(x_train, [4] * 561)
-x_train = np.array(
-    [
-        data_discretization(features, intervals[i])
-        for i, features in enumerate(x_train.T)
-    ]
-).T
-x_test = np.array(
-    [
-        data_discretization(features, intervals[i])
-        for i, features in enumerate(x_test.T)
-    ]
-).T
-if verbose:
-    print(f"Testing discrete naive bayes classifier")
-    print("result of classification of the test set:")
-good = 0
-total = 0
-nb = NaiveBayes()
-nb.fit(x_train, y_train - 1)
-predictions = nb.predict(x_test)
-
-if verbose:
-    print(f"Accuracy: {sum(predictions == y_test - 1) / len(predictions)}")
-"""
